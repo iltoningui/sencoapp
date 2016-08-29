d7efab4ac40c02e43a7e8a5577bcde87ae4eadda
Ilton Flora Ingui <iltoningui@outlook.com>
2016-08-29 21:48:46 +0100

    start-page

---
+++ app/views/welcomes/_welcome.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! welcome, :id, :nome, :created_at, :updated_at
+json.url welcome_url(welcome, format: :json)
---
+++ db/migrate/20160829204603_create_welcomes.rb
@@ -0,0 +1,9 @@
+class CreateWelcomes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :welcomes do |t|
+      t.string :nome
+
+      t.timestamps
+    end
+  end
+end
