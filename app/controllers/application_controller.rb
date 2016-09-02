496231b68c7e3fd81e007ef0d42c9b6d3aff1cae
cabanga <manuelcabanga@hotmail.com>
2016-09-02 15:32:42 +0100

    conclusao do chackout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:nome, :email, :telemovel,:password, :password_confirmation, :morador_id, roles: []) }
+    devise_parameter_sanitizer.permit(:edit) { |u| u.permit(:nome, :email, :telemovel,:password, :password_confirmation, :morador_id, roles: []) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:nome, :email, :telemovel,:password, :password_confirmation, :morador_id, roles: [])}
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   root to: "gerais#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   patch 'gerais/:id/add_to_cart' => 'gerais#add_to_cart', :as => 'add_to_cart'
+  patch 'carrinhos/:id/fazer_comprar' => 'carrinhos#fazer_comprar', :as => 'fazer_comprar'
+
+  
 end
